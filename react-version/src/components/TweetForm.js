ea44302ec9c87e7a69b21404f8d50509d2387de6
Tom <chiatunglo@hotmail.com>
2022-08-30 22:44:46 -0400

    create simple version of Tweeter project using React

---
+++ react-version/src/components/TweetForm.js
@@ -0,0 +1,32 @@
+function TweetForm() {
+  const tweetPlaceholder = "What are you humming about?";
+  return (
+    <section className="newtweet">
+      <form
+        method="post"
+        action="/tweets"
+        className="newtweet__form"
+        onSubmit={(event) => {
+          event.preventDefault();
+        }}
+      >
+        <textarea
+          className="form__textarea"
+          name="text"
+          placeholder={tweetPlaceholder}
+        ></textarea>
+        <input
+          type="submit"
+          value="Tweet"
+          className="form__input"
+          onClick={(event) => {
+            event.preventDefault();
+          }}
+        />
+        <span className="form__counter">140</span>
+      </form>
+    </section>
+  );
+}
+
+export default TweetForm;
---
+++ react-version/src/components/TweetList.js
@@ -0,0 +1,12 @@
+import Tweet from "./Tweet.js";
+
+function TweetList() {
+  return (
+    <section className="tweets">
+      <Tweet />
+      <Tweet />
+    </section>
+  );
+}
+
+export default TweetList;
